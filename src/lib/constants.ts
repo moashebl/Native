ec3b1dc35789abc2a6fa36cb5389cd6477622d61
moashebl <230504583@st.atlas.edu.tr>
2025-07-14 20:14:24 +0300

    03layout

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Aldora';
+export const APP_SLUGAN = process.env.NEXT_PUBLIC_APP_SLUGAN || 'Spend less, save more, enjoy';
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'The best furniture store in Egypt';
+export const APP_KEYWORDS = process.env.NEXT_PUBLIC_APP_KEYWORDS || 'furniture, home decor, Egypt, online shopping';
+export const APP_URL = process.env.NEXT_PUBLIC_APP_URL || 'https://aldora-house.com';
