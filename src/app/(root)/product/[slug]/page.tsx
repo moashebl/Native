035bdedcbefe0441ef040c36b3a70bfcff6c9022
moashebl <230504583@st.atlas.edu.tr>
2025-08-14 00:07:34 +0300

    rating

--- src/app/(root)/product/[slug]/page.tsx
+++ src/app/(root)/product/[slug]/page.tsx
@@ -9,11 +9,14 @@ import ProductPrice from '@/components/shared/product/product-price'
 import ProductGallery from '@/components/shared/product/product-gallery'
 import { Separator } from '@/components/ui/separator'
 import ProductSlider from '@/components/shared/product/product-slider'
-import Rating from '@/components/shared/product/rating'
+import RatingSummary from '@/components/shared/product/rating-summary'
+import ReviewList from './review-list'
 import BrowsingHistoryList from '@/components/shared/browsing-history-list'
 import AddToBrowsingHistory from '@/components/shared/product/add-to-browsing-history'
 import { generateId, round2 } from '@/lib/utils'
 import AddToCart from '@/components/shared/product/add-to-cart'
+import { auth } from '@/../../auth'
+
 
 export async function generateMetadata(props: {
   params: Promise<{ slug: string }>
@@ -48,6 +51,7 @@ export default async function ProductDetails(props: {
     productId: product._id,
     page: Number(page || '1'),
   })
+  const session = await auth()
 
   return (
     <div>
@@ -67,9 +71,13 @@ export default async function ProductDetails(props: {
                 {product.name}
               </h1>
               <div className='flex items-center gap-2'>
-                <span>{product.avgRating.toFixed(1)}</span>
-                <Rating rating={product.avgRating} />
-                <span>{product.numReviews} ratings</span>
+              <RatingSummary
+                avgRating={product.avgRating}
+                numReviews={product.numReviews}
+                asPopover
+                ratingDistribution={product.ratingDistribution}
+              />
+
               </div>
               <Separator />
               <div className='flex flex-col gap-3 sm:flex-row sm:items-center'>
@@ -141,7 +149,12 @@ export default async function ProductDetails(props: {
           </div>
         </div>
       </section>
-
+      <section className='mt-10'>
+        <h2 className='h2-bold mb-2' id='reviews'>
+          Customer Reviews
+        </h2>
+        <ReviewList product={product} userId={session?.user.id} />
+      </section>
       <section className='mt-10'>
         <ProductSlider
           products={relatedProducts.data}
--- src/lib/db/seed.ts
+++ src/lib/db/seed.ts
@@ -4,6 +4,7 @@ import Product from './models/product.model'
 import { cwd } from 'process'
 import { loadEnvConfig } from '@next/env'
 import User from './models/user.model'
+import Review from './models/review.model'
 
 loadEnvConfig(cwd())
 
@@ -12,8 +13,7 @@ const main = async () => {
     // Connect to database first
     await connectToDatabase(process.env.MONGODB_URI)
     
-    const { products, users } = data
-    
+    const { products, users, reviews } = data    
     // Clear existing data
     await User.deleteMany()
     await Product.deleteMany()
@@ -21,10 +21,32 @@ const main = async () => {
     // Insert new data
     const createdUser = await User.insertMany(users)
     const createdProducts = await Product.insertMany(products)
-
+    await Review.deleteMany()
+    const rws = []
+    for (let i = 0; i < createdProducts.length; i++) {
+      let x = 0
+      const { ratingDistribution } = createdProducts[i]
+      for (let j = 0; j < ratingDistribution.length; j++) {
+        for (let k = 0; k < ratingDistribution[j].count; k++) {
+          x++
+          rws.push({
+            ...reviews.filter((x) => x.rating === j + 1)[
+              x % reviews.filter((x) => x.rating === j + 1).length
+            ],
+            isVerifiedPurchase: true,
+            product: createdProducts[i]._id,
+            user: createdUser[x % createdUser.length]._id,
+            updatedAt: Date.now(),
+            createdAt: Date.now(),
+          })
+        }
+      }
+    }
+    const createdReviews = await Review.insertMany(rws)
     console.log({
       createdUser,
       createdProducts,
+      createdReviews,
       message: 'Seeded database successfully',
     })
     process.exit(0)
